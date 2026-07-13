96bd4147d6f77a7cd8c515d117d482fa2c9ffa01
LeanderIneichen <leander.ineichen@stud.msw.ch>
2026-07-13 08:25:49 +0200

    ProgrammErstellt

---
+++ CasioUhrLeander/CasioUhrLeander/main.c
@@ -0,0 +1,18 @@
+/*
+ * CasioUhrLeander.c
+ *
+ * Created: 13.07.2026 08:24:56
+ * Author : e4Ineich
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
